09aa94849427acf74cbb31e9d69f83049621bbcb
root <root@galaxy.pritta.org>
2012-12-17 21:58:55 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '23836768ab185a039bcad58c6e13f4577368fe2411d9277db2c5684d90f2e318c19f42acf409052f0c5e8615f43234a81a629d96083b6595b33a557bbaa52ad7'
